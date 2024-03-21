79220d684ee6b3ace2cb383c29f055a956dc82a7
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-21 14:31:08 +0100

    Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'dark': '#242424',
+        'white': '#F8F8F8',
+        'gray': '#D0D0D0',
+        'primary': '#1C7BC9',
+        'secondary' : '#B1F11A'
+      }
+    },
   },
   plugins: [],
 }
