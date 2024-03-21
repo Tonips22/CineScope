7058e1c8d5d409b0132924518faf3f005ed9050e
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-21 21:28:04 +0100

    Add toggle dark mode 🌙

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from './components/Header.jsx'
 
 function App() {
   return (
     <>
+    <Header></Header>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './Header.css'
+
+export function Header(){
+    const [theme, setTheme] = useState(()=>{
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            return 'dark'
+        }
+
+        return 'light'
+    })
+
+    useEffect(() =>{
+        if(theme === "dark"){
+            document.querySelector('html').classList.add("dark")
+        }else{
+            document.querySelector('html').classList.remove("dark")
+        }
+    }, [theme])
+
+    const handleChangeTheme = () => {
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light");
+    }
+
+    return (
+        <header className=" flex flex-row items-center justify-between px-8 py-4">
+            <a href="\"><h1 className="text-3xl"><span className="font-bold text-primary">Cine</span>Scope</h1></a>
+
+            <nav className=' flex flex-row items-center justify-center gap-4'>
+                <label class="switch">
+                <input type="checkbox" checked={theme === "dark"} onChange={handleChangeTheme}/>
+                    <span class="slider"></span>
+                </label>
+                
+                <a href="https://github.com/Tonips22" target="blank" className=' grid content-center text-3xl text-white hover:text-primary hover:scale-110 transition-all'><i class="fa-brands fa-github "></i></a>
+            </nav>
+        </header>
+    )
+}
+    
+export default Header;
