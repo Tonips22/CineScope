d780ba69e572545645bf685596d4b305c1b09650
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-25 14:08:03 +0100

    Fix Dune video route

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ export function Hero (){
 
     return(
         <section className="relative h-screen flex flex-col items-center justify-center bg-gradient-to-b from-transparent from-30% to-dark to-80%">
-            <video src="./public/Dune.mp4" loop muted autoPlay className=' absolute top-0 left-0 w-full h-full -z-10 object-cover object-center '></video>
+            <video src="./Dune.mp4" loop muted autoPlay className=' absolute top-0 left-0 w-full h-full -z-10 object-cover object-center '></video>
             <Search width={'1/2'}></Search>
         </section>
     )
