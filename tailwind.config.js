cdf46e5899fabd19ae598ad7064ed2673abbf046
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-25 14:00:15 +0100

    Add tailwind animations library @midudev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 export default {
   darkMode: 'selector',
   content: [
@@ -16,6 +18,6 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [animations],
 }
 
