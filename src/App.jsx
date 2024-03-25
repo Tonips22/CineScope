a985db5ffb2b690a687b10b85205bca25dbc184a
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-25 14:03:39 +0100

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 import { Header } from './components/Header.jsx'
+import { Hero } from './sections/Hero.jsx'
 
 function App() {
   return (
     <>
     <Header></Header>
+    <Hero></Hero>
     </>
   )
 }
