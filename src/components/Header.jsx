203b43b61da217962bb4d0acbeff26c5119b6928
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-26 14:22:20 +0100

    Fix header position to absolute

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,16 +23,16 @@ export function Header(){
     }
 
     return (
-        <header className="fixed top-0 left-0 w-full z-50 flex flex-row items-center justify-between px-8 py-4  animate-fade-in-up">
+        <header className="absolute top-0 left-0 w-full z-50 flex flex-row items-center justify-between px-8 py-4  animate-fade-in-up">
             <a href="\"><h1 className="text-3xl text-[#F8F8F8]"><span className="font-bold text-primary">Cine</span>Scope</h1></a>
 
             <nav className=' flex flex-row items-center justify-center gap-4'>
-                <label class="switch">
+                <label className="switch">
                 <input type="checkbox" checked={theme === "dark"} onChange={handleChangeTheme}/>
-                    <span class="slider"></span>
+                    <span className="slider"></span>
                 </label>
                 
-                <a href="https://github.com/Tonips22" target="blank" className=' grid content-center text-3xl text-[#F8F8F8] hover:text-primary hover:scale-110 transition-all'><i class="fa-brands fa-github "></i></a>
+                <a href="https://github.com/Tonips22" target="blank" className=' grid content-center text-3xl text-[#F8F8F8] hover:text-primary hover:scale-110 transition-all'><i className="fa-brands fa-github "></i></a>
             </nav>
         </header>
     )
