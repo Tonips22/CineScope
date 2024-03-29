7e1dade038addcba80e9637781dba0a5e7735592
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-29 13:22:46 +0100

    Add Media query to content section

--- src/sections/Content.jsx
+++ src/sections/Content.jsx
@@ -3,7 +3,7 @@ import { FilmCard } from "../components/FilmCard"
 export function Content(){
     return(
 
-        <section className="res-content grid grid-cols-3 place-content-center min-h-full mx-0 my-auto gap-4">
+        <section className="p-4 res-content grid grid-cols-2 place-content-center min-h-full mx-0 my-auto gap-4 lg:grid-cols-3 ">
         </section>
     )
 }
