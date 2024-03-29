23b7eb11c0af01174e6bf9d4a3f716b4044da00c
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-03-29 11:55:06 +0100

    Add content section

---
+++ src/sections/Content.jsx
@@ -0,0 +1,9 @@
+import { FilmCard } from "../components/FilmCard"
+
+export function Content(){
+    return(
+
+        <section className="res-content grid grid-cols-3 place-content-center min-h-full mx-0 my-auto gap-4">
+        </section>
+    )
+}
