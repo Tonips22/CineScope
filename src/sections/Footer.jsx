4478657ed2f9ea51700bd4811a3f73ed3e4ade6d
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-07-26 09:55:53 +0200

    Add github icon linked to repository

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,15 +4,18 @@ export function Footer(){
 
     return (
         <footer className=" flex flex-row items-center justify-between p-4 bg-primary text-neutral-50">
-            <p>&copy;{year} <span className="text-secondary font-bold">Cine</span>Scope</p>
 
             <div className="flex flex-row items-center justify-center gap-2">
-                <p>Powered by</p>
+                <p className="text-xs">Powered by</p>
                 <a href="https://www.themoviedb.org/?language=es" target="_blank">
-                    <img className="max-w-10" src="./tmdb-logo.png" alt="The Movie Data Base logo" />
+                    <img className="max-w-8" src="./tmdb-logo.png" alt="The Movie Data Base logo" />
                 </a>
-                    
+
             </div>
+
+            <p>&copy;{year} <span className="text-secondary font-bold">Cine</span>Scope</p>
+                    
+            <a href="https://github.com/Tonips22/CineScope" target="blank" className=' grid content-center text-2xl text-[#F8F8F8] hover:text-secondary hover:scale-110 transition-all'><i className="fa-brands fa-github "></i></a>
         </footer>
     )
 }
