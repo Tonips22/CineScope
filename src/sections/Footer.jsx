ae299d2ec599f9ba27fe56b4ed9ea1272e233b30
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-07-26 16:20:41 +0200

    Fix icon size

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -15,7 +15,7 @@ export function Footer(){
 
             <p>&copy;{year} <span className="text-secondary font-bold">Cine</span>Scope</p>
                     
-            <a href="https://github.com/Tonips22/CineScope" target="blank" className=' grid content-center text-3xl text-[#F8F8F8] hover:text-secondary hover:scale-110 transition-all'><i className="fa-brands fa-github "></i></a>
+            <a href="https://github.com/Tonips22/CineScope" target="blank" className=' grid content-center text-2xl text-[#F8F8F8] hover:text-secondary hover:scale-110 transition-all'><i className="fa-brands fa-github "></i></a>
         </footer>
     )
 }
