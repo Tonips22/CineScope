389e31fc213ad85a8c41288e6f6e4291d4a1163e
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-08-05 21:04:52 +0200

    Add movie modal to see more info ✏️

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
+import { MovieModal } from "./MovieModal";
+
 export function MovieCard ({children, description, rating, picture}){
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const ratingColor = () =>{
         if(rating > 7){
@@ -9,8 +14,19 @@ export function MovieCard ({children, description, rating, picture}){
             return "bg-red-500"
         }
     }
+
+    const openModal = () => {
+        setIsModalOpen(true);
+        document.body.style.overflow = 'hidden';
+      };
+    
+      const closeModal = (e) => {
+        e.stopPropagation();
+        setIsModalOpen(false);
+        document.body.style.overflow = 'auto';
+      };
     return (
-        <article className="relative h-[50vh] w-auto group cursor-pointer overflow-hidden lg:h-screen">
+        <article className="relative h-[50vh] w-auto group cursor-pointer overflow-hidden lg:h-screen" onClick={openModal}>
             <img loading="lazy" src={`https://image.tmdb.org/t/p/w500${picture}`} alt={children} className="absolute top-0 left-0 h-full w-full -z-10 object-cover group-hover:scale-110 transition"/>
             <p className={`absolute top-2 right-2 text-[#242424] max-w-[60ch] px-4 py-2 rounded-full ${ratingColor()}`}><i className="fa-solid fa-star"></i> {rating}</p>
 
@@ -19,6 +35,20 @@ export function MovieCard ({children, description, rating, picture}){
                 <p className="text-[#F8F8F8] max-w-[60ch]">{description}</p>
                 <p className={`text-[#242424] max-w-[60ch] px-4 py-2 rounded-full ${ratingColor()}`}><i className="fa-solid fa-star"></i> {rating}</p>
             </div>
+
+            {isModalOpen && (
+                <MovieModal
+                open={isModalOpen}
+                picture={picture}
+                rating={rating}
+                description={description}
+                ratingColor={ratingColor()}
+                onClose={closeModal}
+                >
+                    {children}
+                </MovieModal>
+            )}
         </article>
+
     )
 }
