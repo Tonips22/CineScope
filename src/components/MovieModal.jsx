47405e68aea8fe9f1e55ec79e92ff125abf08e45
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-08-05 21:28:22 +0200

    Make modal fit the full window

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -31,27 +31,25 @@ export function MovieModal({children, description, rating, ratingColor, picture,
 
 
     return (
-        <div className='fixed top-16 left-16 right-16 bottom-16 backdrop-blur-md bg-neutral-50/80 dark:bg-black/80 grid grid-cols-3 animate-fade-in animate-duration-200 z-10 cursor-default'>
-            <img className='col-span-1 w-full h-full object-cover' src={`https://image.tmdb.org/t/p/w500${picture}`} alt={children}/>
+        <div className='fixed top-0 left-0 w-full h-full backdrop-blur-md bg-neutral-50/80 dark:bg-black/80 grid grid-cols-3 animate-fade-in animate-duration-200 z-10 cursor-default'>
+            <img className='col-span-1 w-full max-h-full object-cover' src={`https://image.tmdb.org/t/p/w500${picture}`} alt={children}/>
 
             <div className="col-span-2 p-10 flex flex-col justify-between">
-                <div className="col-span-2 flex flex-col justify-start gap-12">
-                    <h1 className="text-white text-5xl text-center">{children}</h1>
+                <h1 className="text-white text-5xl text-center">{children}</h1>
 
-                    <p className="text-white">{description}</p>
+                <p className="text-white">{description}</p>
 
-                    <p className="text-white text-sm flex gap-2 items-center"><i class="fa-solid fa-calendar"></i>{formartedDate}</p>
+                <p className="text-white text-sm flex gap-2 items-center"><i class="fa-solid fa-calendar"></i>{formartedDate}</p>
 
-                    <p className={`text-[#242424] max-w-[60ch] px-4 py-2 rounded-full ${ratingColor} text-center self-center`}><i className="fa-solid fa-star"></i> {rating}</p>
-                </div>
+                <p className={`text-[#242424] max-w-[60ch] px-4 py-2 rounded-full ${ratingColor} text-center self-center`}><i className="fa-solid fa-star"></i> {rating}</p>
 
-                <p className='text-white flex items-center justify-center gap-2 text-sm'>
+                <p className='text-white flex items-center justify-center gap-2 text-xs'>
                     Information powered by 
                     <img className='max-w-20' src="./tmdblogo.svg" alt="TMDB logo" />
                 </p>
             </div>
 
-            <i class="fa-solid fa-xmark absolute right-2 top-2 text-white text-2xl cursor-pointer z-20" onClick={onClose}></i>
+            <i class="fa-solid fa-xmark absolute right-4 top-4 text-white text-2xl cursor-pointer z-20" onClick={onClose}></i>
       </div>
     )
 }
